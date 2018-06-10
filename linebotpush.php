5da5928708c2c2247b8417832b7c8e73f0407e26
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-10 19:57:52 +0700

    Update linebotpush.php

--- linebotpush.php
+++ linebotpush.php
@@ -35,7 +35,7 @@
     }
     #ตัวอย่าง Message Type "Image"
       else if($message == "รูปน้องแมว"){
-        $image_url = "http://www.catdumb.com/wp-content/uploads/2016/01/Smalls1.jpg";
+        $image_url = "http://catdumb.com/wp-content/uploads/2016/01/Smalls1.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
         $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
