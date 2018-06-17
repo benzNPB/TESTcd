8734ae83be07382711e1b46c655d3744b2ea2ad0
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-17 22:02:20 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo "Hello LINE BOT";
+require_once __DIR__ . '/lineBot.php';
+
+$bot = new Linebot();
+$text = $bot->getMessageText();
+$bot->reply($text);
+$userId = $bot->getUserId();
+$bot->pushText($userId, 'Hello Simple Text!');
+
+
---
+++ setting.php
@@ -0,0 +1,20 @@
+<?php
+
+class Setting {
+	public function getChannelAccessToken(){
+		$channelAccessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";
+		return $channelAccessToken;
+	}
+	public function getChannelSecret(){
+		$channelSecret = "5c1c1cb6d2769f4783b63c8ddddd8f82";
+		return $channelSecret;
+	}
+	public function getApiReply(){
+		$api = "https://api.line.me/v2/bot/message/reply";
+		return $api;
+	}
+	public function getApiPush(){
+		$api = "https://api.line.me/v2/bot/message/push";
+		return $api;
+	}
+}
