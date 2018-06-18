6b48eed390439cece1a3a48877be54627f7069e5
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-18 09:55:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText();
 $bot->reply($text);
-$userId = $bot->getUserId();
-$bot->reply($userId);
+//$userId = $bot->getUserId();
+//$bot->reply($userId);
 
 
 ?>
