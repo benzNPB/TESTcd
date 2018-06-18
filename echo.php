dd909d100765147c72d0785537f619ee31ea6e4d
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-18 10:27:19 +0700

    Add files via upload

--- echo.php
+++ echo.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText();
 if($text == "สวัสดี"){
-	$text = "สวัสดีจ้าาา"
-	$bot->reply($text);
+	 $arrayPostData['messages'][0]['type'] = "text";
+     $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+	$bot->reply($arrayPostData);
     }
