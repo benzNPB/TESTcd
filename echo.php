c4c63ae626d0b8b8d8882568903df14922af6919
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-18 10:59:36 +0700

    Update echo.php

--- echo.php
+++ echo.php
@@ -1,5 +1,10 @@
 <?php
 require_once __DIR__ . '/lineBot.php';
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$channelAccessToken}";
 $bot = new Linebot();
 $text = $bot->getMessageText();
 if($text == "สวัสดี")
