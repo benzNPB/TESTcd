20f32c189f49fe43b61878ebb2b6ccac5cb7cef9
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-18 10:09:03 +0700

    Update echo.php

--- echo.php
+++ echo.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText();
 if($text == "สวัสดี"){
-	$reply == "สวัสดีจ้าาา"
-       $bot->reply($reply);
+	$rply == "สวัสดีจ้าาา"
+       $bot->reply($rply);
     }
 else {
 	
