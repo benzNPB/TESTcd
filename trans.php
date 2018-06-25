b5eee3b9372fadce47c00b90a18758cb2ced933a
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-25 10:58:25 +0700

    Add files via upload

--- trans.php
+++ trans.php
@@ -1,13 +1,48 @@
 <?php
- require_once "GoogleTranslate.php";
- require_once __DIR__ . '/lineBot.php';
-
-//$text = $bot->getMessageText();
-//$bot->reply($text);
-
-$text = $_REQUEST['word'];
-$GT = NEW GoogleTranslate();
-$response = $GT->translate('th','en',$text);  /// ตรง en เราสามารถเปลี่ยนเป็น ภาษาอื่นได้
+    $accessToken = "yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    require_once __DIR__ . '/lineBot.php';
+    require_once "GoogleTranslate.php";
+	$bot = new Linebot();
+	$text = $bot->getMessageText();
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    
+    //รับข้อความจากผู้ใช้
+    $message = $arrayJson['events'][0]['message']['text'];
+#ตัวอย่าง Message Type "Text"
+    $id = $arrayJson['events'][0]['source']['userId'];
+     if($message == "นับ 1-10"){
+       for($i=1;$i<=10;$i++){
+        $arrayPostData['to'] = $id;
+        $word = $_REQUEST['word'];
+		$GT = NEW GoogleTranslate();
+		$response = $GT->translate('th','en',$word);  /// ตรง en เราสามารถเปลี่ยนเป็น ภาษาอื่นได้
 //echo "<pre>";
-echo $text."   =   ".$response;
+		$word."   =   ".$response;
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = $word."   =   ".$response;
+
+          replyMsg($arrayHeader,$arrayPostData);
+
+       }
+    }
+
+    function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+        
+   exit;
 ?>
