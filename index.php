202399e2b49dab79af55411a342c5d345c6a86a5
benzNPB <39670619+benzNPB@users.noreply.github.com>
2018-06-30 17:12:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include ('line-bot-api/php/line-bot.php');
+include ('line-bot.php');
 
 $channelSecret = '5c1c1cb6d2769f4783b63c8ddddd8f82';
 $access_token  = 'yQw5mqImEwMHcau8Hb9CXnPQaTlz11cUCGhUZL64yG1GyAyMJddLMqfjiLwlZgvKfdC2yo896ykJVwW8Xne9++3BjCqj9xsNEdeENjtWVda5UTFIw149B2ygMnCp/4Fcn/nAV1YYOX1YLNxEJkiHwwdB04t89/1O/w1cDnyilFU=';
